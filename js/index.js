bef5f864d457198a35e673f60522198531cd7a7d
RichoDemus <git@richodemus.com>
2020-03-14 15:25:36 +0100

    Create project from rust-wasm-plugin template

---
+++ js/index.js
@@ -0,0 +1,13 @@
+async function load() {
+    const {greet} = await import("../pkg/index.js")
+        // .then(obj => {
+        //     const value = obj.greet();
+        //     console.log("from js:",value);
+        //     return obj;
+        // })
+        .catch(console.error);
+
+    console.log("also js:", greet());
+}
+
+load();
